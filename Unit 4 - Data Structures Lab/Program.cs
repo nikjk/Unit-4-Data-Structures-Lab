fe19accf299d4bf9afb437406ebada3b6d98728f
nikjk <nik.kuehn@outlook.com>
2023-04-30 22:14:44 -0400

    Add project files.

---
+++ Unit 4 - Data Structures Lab/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using Unit_4___Data_Structures_Lab;
+
+Console.WriteLine("Enter a word to reverse:");
+string input = Console.ReadLine();
+Console.WriteLine("Reversed word:");
+ReverseWord reverseword = new ReverseWord(input);
+
+
---
+++ Unit 4 - Data Structures Lab/ReverseWord.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit_4___Data_Structures_Lab
+{
+    public class ReverseWord
+    {
+        public ReverseWord(string input)
+        {
+            char[] inputCharArray = input.ToCharArray();
+            Array.Reverse(inputCharArray);
+            foreach (char c in inputCharArray)
+            {
+                Console.Write(c);
+            }
+        }
+    }
+}
